0b460c00b360738b41edbdc747357791755c885b
K1NGAARON <gerardaaron1997@gmail.com>
2022-12-07 11:00:04 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const errorMSG = $('.woocommerce-notice.woocommerce-notice--info.woocommerce-info');
 
 function replaceErrorText(e) {
-    console.log(errorMSG);
+    // console.log(errorMSG);
 
     if (errorMSG) {
         errorMSG.innerText = 'ups, it seems you have run out of coins from your Lansweeper budget. If you want more coins, pls contact Georgina Rozario. You will be invoiced for these coins.'
-        console.log(errorMSG.innerText);
+        // console.log(errorMSG.innerText);
     } else {
         // Nothing
     }
