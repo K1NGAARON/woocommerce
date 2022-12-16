4a79fd2eb0517fe8809f9359d7ef1d0412baf3fc
K1NGAARON <gerardaaron1997@gmail.com>
2022-12-16 13:12:53 +0100

    size chart script

--- js/lansweeper.js
+++ js/lansweeper.js
@@ -22,8 +22,17 @@ function accountBalance(e) {
     balanceTarget.prepend(html);
 };
 
+function sizeChartText(e) {
+    const title = document.querySelector('.description_tab a');
+
+    if (title) {
+        title.innerHTML = 'Size Chart';
+    }
+}
+
 $(document).ready(function() {
     accountBalance();
+    sizeChartText();
 });
 
 setTimeout(function() {
