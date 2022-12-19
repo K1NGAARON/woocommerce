b8cbac772de155793ce87afef1e21875aca3ff9f
K1NGAARON <gerardaaron1997@gmail.com>
2022-12-19 09:13:31 +0100

    changes to checkout error message

--- js/lansweeper.js
+++ js/lansweeper.js
@@ -2,7 +2,7 @@ function replaceErrorText(e) {
     const errorMSG = document.querySelector('.woocommerce-notice.woocommerce-notice--info.woocommerce-info');
 
     if (errorMSG) {
-        errorMSG.innerText = 'ups, it seems you have run out of coins from your Lansweeper budget. If you want more coins, pls contact Georgina Rozario. You will be invoiced for these coins.';
+        errorMSG.innerText = 'Oops, it seems you have run out of coins from your Lansweeper budget. If you want more coins, pls contact Georgina Rozario. You will be invoiced for these coins.';
         
     } else {
         // Nothing
